af12fa88f1a4c13c113bf01abf9411a701a75960
Rui Chen <rui@meetup.com>
2020-07-09 16:35:57 -0400

    Fix sha reference issue
    `$id` => `\"$id\"` for sha reference fix
    
    error message as
    ```
    {"message":"Problems parsing JSON","documentation_url":"https://developer.github.com/v3/repos/#create-a-repository-dispatch-event"}
    ```
    
    test run log, https://github.com/chenrui333/github-action-test/runs/855606295

--- scripts/repository-dispatch-trigger.sh
+++ scripts/repository-dispatch-trigger.sh
@@ -3,12 +3,12 @@
 set -e
 
 main() {
-  local id=321
+  local id="962368b5c27b7ec5ecfcb4ed8f8c57adb52a8442"
   curl -X POST -u "chenrui333:${GITHUB_TOKEN}" \
     -H "Accept: application/vnd.github.everest-preview+json"  \
     -H "Content-Type: application/json" \
     https://api.github.com/repos/chenrui333/github-action-test/dispatches \
-    --data "{\"event_type\":\"trigger-event\",\"client_payload\":{\"id\":$id, \"unit\":false,\"integration\":true}}"
+    --data "{\"event_type\":\"trigger-event\",\"client_payload\":{\"id\":\"$id\", \"unit\":false,\"integration\":true}}"
 }
 
 main "$@"
