c62b35c20ea15e51be823ca813435c76dd219103
Rui Chen <rui@meetup.com>
2020-11-19 22:12:09 -0500

    update test
    Signed-off-by: Rui Chen <rui@meetup.com>

---
+++ .github/workflows/scripts/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+create-robot-e2e-secret() {
+  echo "Create robot-e2e-secret..."
+  sed -i "s|AUTOBOT_API_KEY_BASE64|${{secrets.AUTOBOT_API_KEY_BASE64}}|g" ./selenium/robot-e2e-secret.yml
+  sed -i "s|MASTER_PASSWORD_BASE64|${{secrets.MASTER_PASSWORD_BASE64}}|g" ./selenium/robot-e2e-secret.yml
+
+  cat ./selenium/robot-e2e-secret.yml
+
+  echo "Finished creating robot-e2e-secret!"
+}
+
+time create-robot-e2e-secret
